545679a5c94daeee0028fc0519351a3cb9559878
DazaWoo <darrenwu5106@gmail.com>
2022-07-28 00:07:28 +0800

    new blog controller and blog index.html.erb、new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,20 @@ Rails.application.routes.draw do
   # (get "/", {to: 'welcome#home'})
   # get "/", {to: 'welcome#home'}
   get "/", to: 'welcome#home' # 到 '/'(首頁) , WelcomeController 找 home action
-  get "/about", to: 'welcome#about'
+  # get "/about", to: 'welcome#about'
+  # get "/about", to: "pages#about" # controller為複數
+  # "/about", to: "pages#about"
+  #get "/about", to: "pages#about"
+
+  resources :blog
+  get "/blog", to: "blog#index"
+  get "/blog/new", to: "blog#new" 
+  post "/blog", to: "blog#create"
 end
+
+# rails generate controller pages
+# rails g controller pages
+
+# 產生
+# pages_controller.rb
+# app/views/pages
