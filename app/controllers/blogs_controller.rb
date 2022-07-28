7130c74882b64ad3384b9dc06f24fb21f5c91ef8
DazaWoo <darrenwu5106@gmail.com>
2022-07-28 13:43:54 +0800

    modify routes and add artice model

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,38 @@
+class BlogsController < ApplicationController
+  def index
+
+  end
+  def new
+
+  end
+  def create
+    # 在同一個 action 下，不能重複 render 或 redirect_to，假如都沒有會出現狀態 204 沒有 content，在 rails 中，204 狀態為無回應。
+    # render html: "已成功新增網誌"
+
+    # 寫入資料庫
+    # redirect_to "/blogs" 
+
+    # render({html: params})
+    render html: params[:content] # params 本身只是看起像 hash ，但本身不是 hash，用字串跟符號都可以取值
+                 #params["content"]
+                #  params.class => ActionController::Parameters
+  end
+end
+
+# symbol 就像一圖騰，看到就會直接聯想到某一個東西， symbol 是一個有名字的物件，就如同數字 2 ，看到就知道是一個數字 2
+# 2 = 3 把 3 指定給 2 ，這樣會錯誤，不能將 3 物件指定給 2 物件
+
+# 建立 model
+=begin
+在資料庫 string => VarChar 字數有上限
+        Text => Text 字數有約 4g 的容量，幾乎用不完
+rails g model Articel title content:text    
+產生
+   article.rb
+   articles 表格(包含 t.string :title, t.text :content, t.timestamps == t.datetime :created/ t.datetime :updated) 共5個欄位(id, title, content, created_at, updated_at)
+$ rails db:migrate => create_table
+class article
+file  article.rb
+table articles
+=end
+# 假如專案還在run，但中途修改檔案 會造成在 terminal 建立其他檔案時會終止不動，是因為 bin/spring cache，要解決需要先將 spring 停下來，輸入 spring stop
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,46 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  # (get "/", {to: 'welcome#home'})
+  # 使用 get方法 到 '/' 網址 , 到 WelcomeController 找 home action
+  # get("/", {to: 'welcome#home'})
   # get "/", {to: 'welcome#home'}
-  get "/", to: 'welcome#home' # 到 '/'(首頁) , WelcomeController 找 home action
+  get "/", to: 'welcome#home' 
   # get "/about", to: 'welcome#about'
   get "/about", to: "pages#about" # controller為複數
   # "/about", to: "pages#about"
   #get "/about", to: "pages#about"
 
-  # resources :blog
-  get "/blog", to: "blog#index"
-  get "/blog/new", to: "blog#new" 
-  post "/blog", to: "blog#create"
+  # 路徑是由上而下逐行比對，new 不能放在 blog/:id 後面
+  # get "/blog", to: "blog#index"
+  # get "/blog/new", to: "blog#new" 
+  # post "/blog", to: "blog#create"
+
+  # get "/blog/:id", to: "blog#show"
+  # 路徑可自行設計 通常會用 :id ， 但是也可以任意更改 改為 :idx 或 :abc 也可以。
+  # get "/blog/:id/hello/:a/:b/:c", to: "blog#show"
+
+  # get "/blog/:id/edit", to: "blog#edit"
+  # patch "/blog/:id", to: "blog#udpate"
+  # delete "/blog/:id", to: "blog#destroy"
+
+  # REST 所有的網址都會被當成一種資源
+  # resources :blogs(用複數 在檢查routes 時，才不會跟 show 衝突，用單數的話 blog#index 的路徑會變為 blog_index)
+  resources :blogs # , path: "helloworld" 假如要修改路徑名稱可用 path "新路徑名稱"
+                   # , only: [:index, :new] # only 只要顯示 ：index, :new 路徑
+                   # , except: [:index, :new] # except 除了 :index, :new 以外，其餘都顯示      
+  # 在終端機找路徑可以用 rails routes -c blog 只找到跟 blog 有關的路徑
 end
+
+# Prefix Verb   URI Pattern              Controller#Action
+# blog_index GET    /blog(.:format)          blog#index
+#            POST   /blog(.:format)          blog#create
+#   new_blog GET    /blog/new(.:format)      blog#new
+#  edit_blog GET    /blog/:id/edit(.:format) blog#edit
+#       blog GET    /blog/:id(.:format)      blog#show
+#            PATCH  /blog/:id(.:format)      blog#update    patch 只能改某一部分
+#            PUT    /blog/:id(.:format)      blog#update    put   可以改整條        通常都會改整條， put 和 patch 都可以
+#            DELETE /blog/:id(.:format)      blog#destroy
+
 
 # rails generate controller pages
 # rails g controller pages
